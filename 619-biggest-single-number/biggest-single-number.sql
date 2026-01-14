fac4add36aa9954abd97ae6aec71c09aadc91935
Arpit Baranwal <64532150+Arpit-Baranwal@users.noreply.github.com>
2026-01-14 22:15:03 +0100

    Time: 386 ms (74.38%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,7 +1,8 @@
-SELECT MAX(num) as num
-FROM (
+WITH SingleOccurrences AS (
     SELECT num 
     FROM MyNumbers 
     GROUP BY num 
     HAVING COUNT(*) = 1
-) AS singles;
+)
+SELECT MAX(num) as num 
+FROM SingleOccurrences;
